7f36d18893a8be5e6dd186c0bdf83cb7086c5978
RUzcategui <redescaluzcategui@gmail.com>
2025-11-26 16:17:41 +0100

    a file

--- src/HemiPlots.jl
+++ src/HemiPlots.jl
@@ -1,7 +1,14 @@
 module HemiPlots
 
 include("composite_types.jl")  
- 
+include("error_handling.jl")  
+include("plot_recipe.jl")  
+include("stereogram.jl")  
+include("stereonets_coordinates.jl") 
 export composite
+export error_handling
+export plot_recipe
+export stereogram
+export stereonets_coordinates
 
 end
