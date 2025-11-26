445cc9f996aee465e42283d8c9a0749717458050
RUzcategui <redescaluzcategui@gmail.com>
2025-11-26 13:48:43 +0100

    test

---
+++ src/composite_types.jl
@@ -0,0 +1,14 @@
+"""
+    composite(s::String)
+
+Esto es una prueba
+
+# Examples
+```julia-repl
+julia> composite("Archivo composite_types.jl")
+Archivo composite_types.jl
+```
+"""
+function composite(s::String)
+    println(s)
+end
