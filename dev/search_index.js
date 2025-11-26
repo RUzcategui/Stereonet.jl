234321e56bf2d4129e043a0e59b3dc3d7b57fccd
Documenter.jl <documenter@juliadocs.github.io>
2025-11-26 14:17:59 +0000

    build based on 3286a2b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HemiPlots","page":"Home","title":"HemiPlots","text":"Documentation for HemiPlots.\n\n","category":"section"},{"location":"#HemiPlots.composite-Tuple{String}","page":"Home","title":"HemiPlots.composite","text":"composite(s::String)\n\nEsto es una prueba\n\nExamples\n\njulia> composite(\"Archivo composite_types.jl\")\nArchivo composite_types.jl\n\n\n\n\n\n","category":"method"}]
+}
